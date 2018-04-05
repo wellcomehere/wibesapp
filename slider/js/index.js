ab9aee5bd679084c3a4983aaf162971ebc2aec68
rocraj <30861179+rocraj@users.noreply.github.com>
2018-04-05 14:13:19 +0530

    wellcomehere website

---
+++ slider/js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+  	pager:false,
+  	mode:'fade',
+  	speed: 500,
+  	slideMargin:2,
+            auto: true,
+            infiniteLoop: true,
+            hideControlOnEnd: true,
+            useCSS: false
+  });
+});
